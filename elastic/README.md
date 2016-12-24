84e5839f90214c9519d635ed8c2ab5483cb113f6
blacktop <blacktop@users.noreply.github.com>
2016-12-23 18:45:17 -0700

    fix entrypoint

---
+++ elastic/README.md
@@ -0,0 +1,15 @@
+elastic  
+=======
+
+TODO
+----
+
+ * [ ] Add kibana index mapping for bro-*
+ * [ ] Add kibana search/vis/dashboards
+ * [ ] Add critical stack  intel
+ * [ ] Add file extraction
+
+NOTES  
+-----
+
+ * https://github.com/TravisFSmith/SweetSecurity
--- elastic/entrypoint.sh
+++ elastic/entrypoint.sh
@@ -9,13 +9,13 @@ fi
 
 if [ "$1" = 'bro' ]; then
 
-  until curl -XGET elasticsearch:9200/; do
+  until curl -s -XGET elasticsearch:9200/; do
     >&2 echo "Failed to configure Elasticsearch, it's unavailable - sleeping 5s"
     sleep 5
   done
 
   >&2 echo "Elasticsearch is up - Set bro mapping"
-  curl -XPUT -H "Content-Type: application/json" --data @template.json elasticsearch:9200/_template/bro
+  curl -s -XPUT -H "Content-Type: application/json" --data @/template.json elasticsearch:9200/_template/bro
 
 	set -- /sbin/tini -- "$@"
 fi
