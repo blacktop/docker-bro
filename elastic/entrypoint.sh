d52596e1db77f1a06307f45a8ced98e36e401bba
blacktop <blacktop@users.noreply.github.com>
2016-12-23 20:43:50 -0700

    format entrypoint

--- elastic/entrypoint.sh
+++ elastic/entrypoint.sh
@@ -25,21 +25,22 @@ if [ "$1" = 'watch' ]; then
 fi
 
 if [ "$1" = 'bro' ]; then
-  until curl -s -XGET elasticsearch:9200/; do
-    >&2 echo "Failed to configure Elasticsearch, it's unavailable - sleeping 5s"
-    sleep 5
-  done
+		until curl -s -XGET elasticsearch:9200/; do
+			  >&2 echo "Failed to configure Elasticsearch, it's unavailable - sleeping 5s"
+			  sleep 5
+		done
 
-  >&2 echo "Elasticsearch is up."
-  >&2 echo "===> Set bro template..."
-  curl -s -XPUT -H "Content-Type: application/json" --data @/template.json 'elasticsearch:9200/_template/bro'
-  >&2 echo "===> Set bro index-pattern..."
-  curl -s -XPUT -H "Content-Type: application/json" --data @/index-pattern.json 'elasticsearch:9200/.kibana/index-pattern/bro-*'
-  >&2 echo "===> Set bro-* as kibana default index..."
-	KIBANA=$(curl -s 'elasticsearch:9200/.kibana/config/_search' | jq -r '.hits.hits[] ._id')
-  curl -s -XPUT "elasticsearch:9200/.kibana/config/$KIBANA" -d '{"defaultIndex" : "bro-*"}'
+		>&2 echo "Elasticsearch is up."
+		>&2 echo "===> Set bro template..."
+		curl -s -XPUT -H "Content-Type: application/json" --data @/template.json 'elasticsearch:9200/_template/bro'
+		>&2 echo "===> Set bro index-pattern..."
+		curl -s -XPUT -H "Content-Type: application/json" --data @/index-pattern.json \
+		'elasticsearch:9200/.kibana/index-pattern/bro-*'
+		>&2 echo "===> Set bro-* as kibana default index..."
+		KIBANA=$(curl -s 'elasticsearch:9200/.kibana/config/_search' | jq -r '.hits.hits[] ._id')
+		curl -s -XPUT "elasticsearch:9200/.kibana/config/$KIBANA" -d '{"defaultIndex" : "bro-*"}'
 
-	set -- /sbin/tini -- "$@"
+		set -- /sbin/tini -- "$@"
 fi
 
 exec "$@"
