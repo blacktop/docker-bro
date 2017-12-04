1780ddbca696b3e2ffd8d2343bba0c03e84ff17a
blacktop <blacktop@users.noreply.github.com>
2017-12-03 20:02:15 -0700

    small tweak

--- tests/kafka.sh
+++ tests/kafka.sh
@@ -16,8 +16,6 @@ docker run -d --rm \
            -v `pwd`/pcap:/pcap \
            blacktop/bro:kafka -F -r heartbleed.pcap local "Site::local_nets += { 192.168.11.0/24 }"
 
-sleep 10; echo "===> Starting kafka consumer..."
-kafka-console-consumer -brokers=localhost:9092 -topic bro -offset=oldest > kafka.out &
-sleep 5; kill %1 > /dev/null 2>&1
-
-cat kafka.out | grep 'Value:' | cut -d ':' -f 2- | jq 'select(.notice != null) | .notice.note'
+sleep 10
+echo "===> Starting kafka consumer..."
+kafka-console-consumer -brokers=localhost:9092 -topic bro -offset=oldest | grep 'Value:' | cut -d ':' -f 2- | jq 'select(.notice != null) | .notice.note'
