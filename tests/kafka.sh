bdf4be9aaf46cf5197f480790575709fe28ecb04
blacktop <blacktop@users.noreply.github.com>
2018-05-12 15:55:25 -0400

    should fix #11

--- tests/kafka.sh
+++ tests/kafka.sh
@@ -1,23 +1,27 @@
 #!/bin/bash
 
 # clean up
-rm kafka.out || true
+rm /tmp/kafka.out || true
 
 echo "===> Starting kafka..."
 docker run -d --name kafka \
            -p 9092:9092 \
            -e KAFKA_ADVERTISED_HOST_NAME=localhost \
            -e KAFKA_CREATE_TOPICS=bro:1:1 \
-           blacktop/kafka:0.11
+           blacktop/kafka:1.1
+
+sleep 10
+echo "===> Starting kafka consumer..."
+kafka-console-consumer --brokers=localhost:9092 --topic=bro | tee -a /tmp/kafka.out &
 
 echo "===> Starting bro..."
-docker run -d --rm \
+docker run --rm \
            --link kafka:localhost \
            -v `pwd`/pcap:/pcap \
            blacktop/bro:kafka -F -r heartbleed.pcap local "Site::local_nets += { 192.168.11.0/24 }"
 
-sleep 10
-echo "===> Starting kafka consumer..."
-docker run --rm --link kafka:localhost ktools
+# sleep 10
+# echo "===> Starting kafka consumer..."
+# docker run --rm --link kafka:localhost ktools
 
-# cat kafka.out | grep 'Value:' | cut -d ':' -f 2- | jq 'select(.notice != null) | .notice.note'
+cat /tmp/kafka.out | grep 'Value:' | cut -d ':' -f 2- | jq 'select(.notice != null) | .notice.note'
